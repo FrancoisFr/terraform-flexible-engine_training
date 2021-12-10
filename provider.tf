ae7c9dafcde96c268aec6276ec0854dbac976929
FrancoisFr <48807558+FrancoisFr@users.noreply.github.com>
2021-12-10 09:08:50 +0000

    without module for the moment

--- provider.tf
+++ provider.tf
@@ -3,15 +3,15 @@ terraform {
 
   required_providers {
     flexibleengine = {
-      source = "FlexibleEngineCloud/flexibleengine"
+      source  = "FlexibleEngineCloud/flexibleengine"
       version = ">= 1.20.0"
     }
   }
 }
 
 provider "flexibleengine" {
-    access_key = var.FLEXIBLE_ENGINE_ACCESS_KEY
-    secret_key = var.FLEXIBLE_ENGINE_SECRET_KEY
-    domain_name = var.DOMAIN_NAME
-    region = "eu-west-0"
+  access_key  = var.FLEXIBLE_ENGINE_ACCESS_KEY
+  secret_key  = var.FLEXIBLE_ENGINE_SECRET_KEY
+  domain_name = var.DOMAIN_NAME
+  region      = "eu-west-0"
 }
