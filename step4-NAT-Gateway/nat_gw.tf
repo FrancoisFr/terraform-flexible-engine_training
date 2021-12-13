ea9729a6a4bb2190d7d22b4f1d537d6b3a81a374
FrancoisFr <{ID}+{username}@users.noreply.github.com>
2021-12-13 11:49:06 +0100

    added SNAT & DNAT rules for public subnet

--- step4-NAT-Gateway/nat_gw.tf
+++ step4-NAT-Gateway/nat_gw.tf
@@ -13,3 +13,18 @@ resource "flexibleengine_nat_gateway_v2" "nat_private" {
   vpc_id      = flexibleengine_vpc_v1.vpc_test.id
   subnet_id   = flexibleengine_vpc_subnet_v1.subnet_private.id
 }
+
+resource "flexibleengine_nat_snat_rule_v2" "snat_public" {
+  nat_gateway_id = flexibleengine_nat_gateway_v2.nat_public.id
+  floating_ip_id = flexibleengine_vpc_eip_v1.eip_SNAT.id
+  subnet_id      = flexibleengine_vpc_subnet_v1.subnet_public.id
+}
+
+resource "flexibleengine_nat_dnat_rule_v2" "dnat_public" {
+  nat_gateway_id        = flexibleengine_nat_gateway_v2.nat_public.id
+  floating_ip_id        = flexibleengine_vpc_eip_v1.eip_DNAT.id
+  port_id               = flexibleengine_compute_instance_v2.instance1-step4.network[0].port
+  protocol              = "tcp"
+  internal_service_port = 22
+  external_service_port = 8080
+}
