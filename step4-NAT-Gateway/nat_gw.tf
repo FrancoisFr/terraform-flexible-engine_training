01639582beb61fb5fde94b08436f78bdf1185f7f
FrancoisFr <{ID}+{username}@users.noreply.github.com>
2021-12-13 11:07:54 +0100

    added a NAT-GW

--- step4-NAT-Gateway/nat_gw.tf
+++ step4-NAT-Gateway/nat_gw.tf
@@ -1,7 +1,15 @@
-resource "flexibleengine_nat_gateway_v2" "nat_1" {
-  name        = "nat_test"
+resource "flexibleengine_nat_gateway_v2" "nat_public" {
+  name        = "nat_test_public"
   description = "test for terraform"
   spec        = "1"
   vpc_id      = flexibleengine_vpc_v1.vpc_test.id
   subnet_id   = flexibleengine_vpc_subnet_v1.subnet_1.id
+}
+
+resource "flexibleengine_nat_gateway_v2" "nat_private" {
+  name        = "nat_test_private"
+  description = "test for terraform"
+  spec        = "1"
+  vpc_id      = flexibleengine_vpc_v1.vpc_test.id
+  subnet_id   = flexibleengine_vpc_subnet_v1.subnet_2.id
 }
