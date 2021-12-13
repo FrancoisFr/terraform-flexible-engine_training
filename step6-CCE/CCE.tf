412ed7777d4d82be0d6f84070ce0290a86688268
FrancoisFr <{ID}+{username}@users.noreply.github.com>
2021-12-13 18:06:26 +0100

    cluster created (removed label)

--- step6-CCE/CCE.tf
+++ step6-CCE/CCE.tf
@@ -1,5 +1,5 @@
-resource "flexibleengine_cce_cluster_v3" "cluster_1" {
-  name                   = "cluster_test"
+resource "flexibleengine_cce_cluster_v3" "clustertest" {
+  name                   = "clustertest"
   cluster_type           = "VirtualMachine"
   authentication_mode    = "rbac"
   description            = "new cluster test"
@@ -7,5 +7,4 @@ resource "flexibleengine_cce_cluster_v3" "cluster_1" {
   vpc_id                 = flexibleengine_vpc_v1.vpc_test_cce.id
   subnet_id              = flexibleengine_vpc_subnet_v1.subnet_cce.id
   container_network_type = "overlay_l2"
-  labels = var.tag_Custom
 }
--- step6-CCE/vpc.tf
+++ step6-CCE/vpc.tf
@@ -3,9 +3,9 @@ resource "flexibleengine_vpc_v1" "vpc_test_cce" {
   cidr = "192.168.0.0/16"
 }
 
-resource "flexibleengine_vpc_subnet_v1" "subnet_1" {
+resource "flexibleengine_vpc_subnet_v1" "subnet_cce" {
   name       = "subnet_cce"
-  vpc_id     = flexibleengine_vpc_v1.vpc_test.id
+  vpc_id     = flexibleengine_vpc_v1.vpc_test_cce.id
   cidr       = "192.168.0.0/24"
   gateway_ip = "192.168.0.1"
 }
