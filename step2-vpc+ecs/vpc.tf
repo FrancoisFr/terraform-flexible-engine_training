21fc5eeb500c8d7d7846c55e2a0f9012681cb261
F.Richard <48807558+FrancoisFr@users.noreply.github.com>
2021-12-21 10:05:02 +0100

    Update vpc.tf

--- step2-vpc+ecs/vpc.tf
+++ step2-vpc+ecs/vpc.tf
@@ -10,4 +10,5 @@ resource "flexibleengine_vpc_subnet_v1" "subnet_1" {
   vpc_id     = flexibleengine_vpc_v1.vpc_test.id
   cidr       = "192.168.0.0/24"
   gateway_ip = "192.168.0.1"
+  tags = var.tag_Custom
 }
