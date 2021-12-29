558ecf777cb3da63e05ae8b4ae4fa6da1dbcc3c7
FrancoisFr <48807558+FrancoisFr@users.noreply.github.com>
2021-12-29 15:00:38 +0100

    added backend for every step

---
+++ step1-basic-ecs/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "bucket-test-francois"
+    region = "eu-west-0"
+    key = "test-francois/step-1/terraform.tfstate"
+    endpoint = "oss.eu-west-0.prod-cloud-ocb.orange-business.com"
+    access_key = "O8HEQDGHG52W0NHEMCOH"
+    secret_key = "3aJ39G3zSu1PgUUCMJaTmxgFtpSZ6FrWqkpUFDGg"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+}
